93d41310bb116066f0c88f276b985951101b9332
wangzhubing <1396850063@qq.com>
2018-08-24 22:47:28 +0800

    test

--- src/main/java/com/wzb/infa/infazipper/App.java
+++ src/main/java/com/wzb/infa/infazipper/App.java
@@ -32,131 +32,139 @@ import com.wzb.infa.properties.InfaProperty;
  *
  */
 public class App {
-	private InfaProperty infaProperty = InfaProperty.getInstance();
-	private Logger log = Logger.getLogger(App.class);
-	// 依赖infaProperty，所以必须在infaProperty的“自定义配置”之后在获取实例
-	private InfaUtils infaUtil = null;
 
-	public static void main(String[] args) throws DocumentException, IOException {
+    private final InfaProperty infaProperty = InfaProperty.getInstance();
+    private final Logger log = Logger.getLogger(App.class);
+    // 依赖infaProperty，所以必须在infaProperty的“自定义配置”之后在获取实例
+    private InfaUtils infaUtil = null;
 
-		App app = new App();
-		if (args.length > 0) {
-			// 自定义配置
-			app.infaProperty.addInfaProperty(args[0]);
-		}
-		// 在infaProperty的“自定义配置”之后infaUtil在获取实例
-		app.infaUtil = new InfaUtils();
+    public static void main(String[] args) throws DocumentException, IOException {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		String beginDate = sdf.format(new Date());
-		app.run();
-		String endDate = sdf.format(new Date());
-		System.out.println(beginDate + "-------" + endDate);
-		// 释放资源
-		app.infaUtil.dbUtil.release();
-		app.log.info("quit 0");
-	}
+        App app = new App();
+        if (args.length > 0) {
+            // 自定义配置
+            app.infaProperty.addInfaProperty(args[0]);
+        }
+        // 在infaProperty的“自定义配置”之后infaUtil在获取实例
+        app.infaUtil = new InfaUtils();
 
-	private void run() throws IOException {
-		// 定义参数
-		String mappingType = infaProperty.getProperty("mapping.type", "-1");
-		HashSet<String> tableList = new HashSet<String>();// 保存要生成的表清单
-		BufferedReader tableListReader = null;// 读取要生成的表名清单的Reader
-		BufferedWriter errorTableLogWriter = null;// 生成报错日志记录
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String beginDate = sdf.format(new Date());
+        app.run();
+        String endDate = sdf.format(new Date());
+        System.out.println(beginDate + "-------" + endDate);
+        // 释放资源
+        app.infaUtil.dbUtil.release();
+        app.log.info("quit 0");
+    }
 
-		String table = null;
-		try {
-			// 初始化错误日志记录文件
-			File errorTableLogTo = new File(infaProperty.getProperty("work.dir")
-					+ infaProperty.getProperty("tables.err_tables", "err_tables.txt"));
+    private void run() throws IOException {
+        // 定义参数
+        String mappingType = infaProperty.getProperty("mapping.type", "-1");
+        HashSet<String> tableList = new HashSet<>();// 保存要生成的表清单
+        BufferedReader tableListReader = null;// 读取要生成的表名清单的Reader
+        BufferedWriter errorTableLogWriter = null;// 生成报错日志记录
 
-			if (!errorTableLogTo.exists()) {
-				//errorTableLogTo.createNewFile();
-			}
+        String table;
+        try {
+            // 初始化错误日志记录文件
+            File errorTableLogTo = new File(infaProperty.getProperty("work.dir")
+                    + infaProperty.getProperty("tables.err_tables", "err_tables.txt"));
 
-			errorTableLogWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(errorTableLogTo)));
+            if (!errorTableLogTo.exists()) {
+                //errorTableLogTo.createNewFile();
+            }
 
-			tableListReader = new BufferedReader(new InputStreamReader(
-					new FileInputStream(infaProperty.getProperty("work.dir") + infaProperty.getProperty("tables"))));
+            errorTableLogWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(errorTableLogTo)));
 
-			while ((table = tableListReader.readLine()) != null) {
-				tableList.add(table.trim().toUpperCase());
-			}
-			// 制作XML
-			int size = tableList.size();
-			int errorSize = 0;
-			int tableCount = 0;// 已经生成XML的表数量
-			infaUtil.xmlFileChange(0);
+            tableListReader = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(infaProperty.getProperty("work.dir") + infaProperty.getProperty("tables"))));
 
-			for (Iterator<String> it = tableList.iterator(); it.hasNext();) {
-				table = it.next();
-				System.out.print("Makeing xml for " + StringPadder.rightPad(table, "-", 30) + "("
-						+ (tableCount + errorSize + 1) + "/" + size + ")");
-				try {
-					if (mappingType.equals("1")) {
-						infaUtil.makeZipperXML(table);
-					} else if (mappingType.equals("2")) {
-						infaUtil.makeAddXML(table);
-					} else{
-						infaUtil.makeTruncateThenDeleteXML(table);
-					}
-					System.out.println("success!");
-					tableCount++;
-				} catch (NoPrimaryKeyException | CheckTableExistException | UnsupportedDatatypeException
-						| SQLException e) {
-					errorSize++;
-					errorTableLogWriter.write(table + " exception info:" + e.getMessage());
-					errorTableLogWriter.newLine();
-					errorTableLogWriter.flush();
-					System.out.println(" exception info:" + e.getMessage());
-					e.printStackTrace();
-				}
-				if ((tableCount % Integer.parseInt(infaProperty.getProperty("xml.output.mappings", "20"))) == 0) {
-					infaUtil.xmlFileWrite(".");
-					int fileCount = Math.floorDiv(tableCount,
-							Integer.parseInt(infaProperty.getProperty("xml.output.mappings", "20")));
-					infaUtil.xmlFileChange(fileCount);
-				}
-			}
-			infaUtil.xmlFileWrite(".");
-		} catch (FileNotFoundException e) {
-			log.warn(e.getMessage());
-		} catch (UnsupportedEncodingException e) {
-			log.warn(e.getMessage());
-		} catch (IOException e) {
-			log.warn(e.getMessage());
-		} catch (NullPointerException e) {
-			infaUtil.xmlFileWrite(".");
-			e.printStackTrace();
-		} finally {
-			try {
-				tableListReader.close();
-				errorTableLogWriter.close();
-			} catch (Exception e) {
-				log.warn(e.getMessage());
-			}
-		}
-	}
+            while ((table = tableListReader.readLine()) != null) {
+                tableList.add(table.trim().toUpperCase());
+            }
+            // 制作XML
+            int size = tableList.size();
+            int errorSize = 0;
+            int tableCount = 0;// 已经生成XML的表数量
+            infaUtil.xmlFileChange(0);
 
-	static class StringPadder {
-		private static StringBuffer strb;
-		private static StringCharacterIterator sci;
+            for (Iterator<String> it = tableList.iterator(); it.hasNext();) {
+                table = it.next();
+                System.out.print("Makeing xml for " + StringPadder.rightPad(table, "-", 30) + "("
+                        + (tableCount + errorSize + 1) + "/" + size + ")");
+                try {
+                    switch (mappingType) {
+                        case "1":
+                            infaUtil.makeZipperXML(table);
+                            break;
+                        case "2":
+                            infaUtil.makeAddXML(table);
+                            break;
+                        default:
+                            infaUtil.makeTruncateThenDeleteXML(table);
+                            break;
+                    }
+                    System.out.println("success!");
+                    tableCount++;
+                } catch (NoPrimaryKeyException | CheckTableExistException | UnsupportedDatatypeException
+                        | SQLException e) {
+                    errorSize++;
+                    errorTableLogWriter.write(table + " exception info:" + e.getMessage());
+                    errorTableLogWriter.newLine();
+                    errorTableLogWriter.flush();
+                    System.out.println(" exception info:" + e.getMessage());
+                }
+                if ((tableCount % Integer.parseInt(infaProperty.getProperty("xml.output.mappings", "20"))) == 0) {
+                    infaUtil.xmlFileWrite(".");
+                    int fileCount = Math.floorDiv(tableCount,
+                            Integer.parseInt(infaProperty.getProperty("xml.output.mappings", "20")));
+                    infaUtil.xmlFileChange(fileCount);
+                }
+            }
+            infaUtil.xmlFileWrite(".");
+        } catch (FileNotFoundException e) {
+            log.warn(e.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            log.warn(e.getMessage());
+        } catch (IOException e) {
+            log.warn(e.getMessage());
+        } catch (NullPointerException e) {
+            infaUtil.xmlFileWrite(".");
+        } finally {
+            try {
+                if (tableListReader != null) {
+                    tableListReader.close();
+                }
+                if (errorTableLogWriter != null) {
+                    errorTableLogWriter.close();
+                }
+            } catch (IOException e) {
+                log.warn(e.getMessage());
+            }
+        }
+    }
 
-		public static String rightPad(String stringToPad, String padder, int size) {
-			if (padder.length() == 0) {
-				return stringToPad;
-			}
-			strb = new StringBuffer(stringToPad);
-			sci = new StringCharacterIterator(padder);
+    static class StringPadder {
 
-			while (strb.length() < size) {
-				for (char ch = sci.first(); ch != CharacterIterator.DONE; ch = sci.next()) {
-					if (strb.length() < size) {
-						strb.append(String.valueOf(ch));
-					}
-				}
-			}
-			return strb.toString();
-		}
-	}
+        private static StringBuffer strb;
+        private static StringCharacterIterator sci;
+
+        public static String rightPad(String stringToPad, String padder, int size) {
+            if (padder.length() == 0) {
+                return stringToPad;
+            }
+            strb = new StringBuffer(stringToPad);
+            sci = new StringCharacterIterator(padder);
+
+            while (strb.length() < size) {
+                for (char ch = sci.first(); ch != CharacterIterator.DONE; ch = sci.next()) {
+                    if (strb.length() < size) {
+                        strb.append(String.valueOf(ch));
+                    }
+                }
+            }
+            return strb.toString();
+        }
+    }
 }
