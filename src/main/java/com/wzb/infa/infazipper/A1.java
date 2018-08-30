f4b90aeda74df89aa6189b7e30dfe4aa7a141026
wangzhubing12 <1396850063@qq.com>
2018-08-30 20:34:15 +0800

    121

--- src/main/java/com/wzb/infa/infazipper/A1.java
+++ src/main/java/com/wzb/infa/infazipper/A1.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.SQLException;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -16,7 +18,6 @@ import com.wzb.infa.dbutils.DbUtil;
 import com.wzb.infa.dbutils.XMLUtil;
 import com.wzb.infa.exceptions.CheckTableExistException;
 import com.wzb.infa.exceptions.UnsupportedDatatypeException;
-import com.wzb.infa.infazipper.App.StringPadder;
 import com.wzb.infa.obj.InfaXML;
 import com.wzb.infa.properties.InfaProperty;
 
@@ -25,12 +26,15 @@ public class A1 {
 
 	public static void main(String[] args) {
 		InfaProperty infaProperty = null;
+		XMLUtil xmlUtil = XMLUtil.getInstance(); // XML工具
+
 		A1 a1 = new A1();
-		HashSet<String> tableLIst = a1.getTableLIst();
-		int size = tableLIst.size();
-		int errorSize = 0;
+		HashSet<String> tableLIst = a1.getTableList(); // 表清单
+		int size = tableLIst.size(); // 总的表数量
+		int errorSize = 0; // 报错的表数量
 		int tableCount = 0;// 已经生成XML的表数量
 
+		// 获取配置文件
 		if (args.length > 0) {
 			try {
 				infaProperty = InfaProperty.getInstance().addInfaProperty(args[0]);
@@ -42,8 +46,7 @@ public class A1 {
 			System.exit(-1);
 		}
 		String mappingType = infaProperty.getProperty("mapping.type", "-1");
-		XMLUtil xmlUtil = XMLUtil.getInstance();
-		ArrayList<InfaXML> xmls = new ArrayList<>();
+		ArrayList<InfaXML> xmls = new ArrayList<>(); // 保存生成的XML
 		for (String table : tableLIst) {
 			log.info("Makeing xml for " + StringPadder.rightPad(table, "-", 30) + "(" + (tableCount + errorSize + 1)
 					+ "/" + size + ")");
@@ -58,6 +61,7 @@ public class A1 {
 
 			tableCount++;
 
+			// 如果生成的表数量达到xx个则切换文件
 			if ((tableCount % Integer.parseInt(infaProperty.getProperty("xml.output.mappings", "20"))) == 0) {
 				int fileCount = Math.floorDiv(tableCount,
 						Integer.parseInt(infaProperty.getProperty("xml.output.mappings", "20")));
@@ -75,7 +79,10 @@ public class A1 {
 		DbUtil.getInstance().release();
 	}
 
-	private HashSet<String> getTableLIst() {
+	/***
+	 * 获取要生成的表清单
+	 */
+	private HashSet<String> getTableList() {
 		HashSet<String> tableList = new HashSet<>();// 保存要生成的表清单
 		BufferedReader tableListReader = null;// 读取要生成的表名清单的Reader
 		try {
@@ -97,8 +104,32 @@ public class A1 {
 			try {
 				tableListReader.close();
 			} catch (IOException e) {
+				log.error("ddd"+e.getMessage());
 			}
 		}
 		return tableList;
 	}
+	
+    static class StringPadder {
+
+        private static StringBuffer strb;
+        private static StringCharacterIterator sci;
+
+        public static String rightPad(String stringToPad, String padder, int size) {
+            if (padder.length() == 0) {
+                return stringToPad;
+            }
+            strb = new StringBuffer(stringToPad);
+            sci = new StringCharacterIterator(padder);
+
+            while (strb.length() < size) {
+                for (char ch = sci.first(); ch != CharacterIterator.DONE; ch = sci.next()) {
+                    if (strb.length() < size) {
+                        strb.append(String.valueOf(ch));
+                    }
+                }
+            }
+            return strb.toString();
+        }
+    }
 }
